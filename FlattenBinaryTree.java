2e221d4322c23d818e98e519564dd071f39f7050
Dalto Curvelano Jr <daltojr@gmail.com>
2020-04-06 01:10:03 +0400

    Flatten binary tree

---
+++ FlattenBinaryTree.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void flatten(TreeNode root) {
+        Queue<TreeNode> q = new ArrayDeque<>();
+        
+        helper(root, q);
+        
+        TreeNode head = q.poll();
+        TreeNode curr = head;
+        
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            curr.left = null;
+            curr.right = node;
+            curr = curr.right;
+        }
+    }
+    
+    public void helper(TreeNode node, Queue<TreeNode> q) {
+        if (node == null) return;
+        q.add(node);
+        helper(node.left, q);
+        helper(node.right, q);
+    }
+}
